2747a375e4b37f2d12b69afe3d6db77b108a2798
toshi199709 <ayatoshi077@gmail.com>
2025-05-13 08:36:56 +0900

    モデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,20 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
+  validates :nickname, presence: true
 
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+  validates :last_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+
+  VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+  validates :last_name_kana, presence: true, format: { with: VALID_KANA_REGEX, message: 'は全角カタカナで入力してください' }
+  validates :first_name_kana, presence: true, format: { with: VALID_KANA_REGEX, message: 'は全角カタカナで入力してください' }
+
+  validates :birthday, presence: true
+
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'は半角英数字を含めてください' }, unless: lambda {
+    password.blank?
+  }
 end
