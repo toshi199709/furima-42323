e51f3efb68c84b32bfac6e5eedb1959d49ff66fb
toshi199709 <ayatoshi077@gmail.com>
2025-05-15 07:51:32 +0900

    アソシエーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  belongs_to_active_hash :category
   belongs_to_active_hash :condition
   belongs_to_active_hash :shipping_fee_status
   belongs_to_active_hash :prefecture
