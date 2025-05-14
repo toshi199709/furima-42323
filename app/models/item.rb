7d6bc28814375d9e4052742709c5bda3b195659a
toshi199709 <ayatoshi077@gmail.com>
2025-05-14 11:53:42 +0900

    商品画像が必要

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,14 +8,22 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :shipping_day
 
-  validates :name, :description, :price, :image, presence: true
+  validates :name, :description, :price, presence: true
   validates :category_id, :condition_id, :shipping_fee_status_id, :prefecture_id, :shipping_day_id,
             numericality: { other_than: 1, message: 'を選択してください' }
-  validates :condition_id, numericality: { other_than: 1, message: 'を選択してください' }
-  validates :shipping_day_id, numericality: { other_than: 1, message: 'を選択してください' }
-  validates :prefecture_id, numericality: { other_than: 1, message: 'を選択してください' }
-  validates :shipping_day_id, numericality: { other_than: 1, message: 'を選択してください' }
-  validates :price, presence: true,
-                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'は¥300〜¥9,999,999の範囲で入力してください' },
-                    format: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' }
+
+  validates :price, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 300,
+    less_than_or_equal_to: 9_999_999,
+    message: 'は¥300〜¥9,999,999の範囲で入力してください'
+  }, format: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' }
+
+  validate :image_presence # ← これ！
+
+  private
+
+  def image_presence
+    errors.add(:image, 'を添付してください') unless image.attached?
+  end
 end
