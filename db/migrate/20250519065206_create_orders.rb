e731b8152947e3ae6e20a383d0dc13ce43278801
toshi199709 <ayatoshi077@gmail.com>
2025-05-20 16:33:56 +0900

    テーブル設計

--- db/migrate/20250519065206_create_orders.rb
+++ db/migrate/20250519065206_create_orders.rb
@@ -1,7 +1,15 @@
 class CreateOrders < ActiveRecord::Migration[7.1]
   def change
     create_table :orders do |t|
-      t.integer :price  ,null: false
+      t.string  :postal_code,     null: false
+      t.integer :prefecture_id,   null: false
+      t.string  :city,            null: false
+      t.string  :address,         null: false
+      t.string  :building
+      t.string  :phone_number,    null: false
+      t.references :item,         null: false, foreign_key: true
+      t.references :user,         null: false, foreign_key: true
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -55,9 +55,18 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_19_065206) do
   end
 
   create_table "orders", charset: "utf8mb3", force: :cascade do |t|
-    t.integer "price", null: false
+    t.string "postal_code", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", null: false
+    t.string "address", null: false
+    t.string "building"
+    t.string "phone_number", null: false
+    t.bigint "item_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_orders_on_item_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
@@ -81,4 +90,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_19_065206) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
+  add_foreign_key "orders", "items"
+  add_foreign_key "orders", "users"
 end
