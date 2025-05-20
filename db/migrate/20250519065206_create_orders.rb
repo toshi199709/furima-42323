4c16ba2b898fe8e8e4ef188082ebe438421890ba
toshi199709 <ayatoshi077@gmail.com>
2025-05-20 18:01:04 +0900

    テーブル修正

--- db/migrate/20250519065206_create_orders.rb
+++ db/migrate/20250519065206_create_orders.rb
@@ -1,14 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[7.1]
   def change
     create_table :orders do |t|
-      t.string  :postal_code,     null: false
-      t.integer :prefecture_id,   null: false
-      t.string  :city,            null: false
-      t.string  :address,         null: false
-      t.string  :building
-      t.string  :phone_number,    null: false
-      t.references :item,         null: false, foreign_key: true
-      t.references :user,         null: false, foreign_key: true
+      t.references :item, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_19_065206) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_20_090013) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -39,6 +39,19 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_19_065206) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "addresses", charset: "utf8mb3", force: :cascade do |t|
+    t.string "postal_code", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", null: false
+    t.string "address", null: false
+    t.string "building"
+    t.string "phone_number", null: false
+    t.bigint "order_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_addresses_on_order_id"
+  end
+
   create_table "items", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.text "description", null: false
@@ -55,12 +68,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_19_065206) do
   end
 
   create_table "orders", charset: "utf8mb3", force: :cascade do |t|
-    t.string "postal_code", null: false
-    t.integer "prefecture_id", null: false
-    t.string "city", null: false
-    t.string "address", null: false
-    t.string "building"
-    t.string "phone_number", null: false
     t.bigint "item_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
@@ -89,6 +96,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_19_065206) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "addresses", "orders"
   add_foreign_key "items", "users"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
