5b7dfa735bb60d9e712efcbceda509b3d6a9a2e9
toshi199709 <ayatoshi077@gmail.com>
2025-05-21 16:44:55 +0900

    priceの削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -38,7 +38,7 @@ class OrdersController < ApplicationController
   def order_address_params
     params.require(:order_address).permit(
       :postal_code, :prefecture_id, :city,
-      :address, :building, :phone_number, :price
+      :address, :building, :phone_number
     ).merge(
       token: params[:token],
       item_id: @item.id,
