df2b32b14b0f500973fea4f09e134f9e5e619734
toshi199709 <ayatoshi077@gmail.com>
2025-05-21 09:37:38 +0900

    コントローラー修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,14 +6,14 @@ class OrdersController < ApplicationController
   before_action :redirect_if_invalid_order, only: [:index, :create]
 
   def index
-    @order = Order.new
+    @order_address = OrderAddress.new
   end
 
   def create
-    @order = Order.new(order_params)
-    if @order.valid?
+    @order_address = OrderAddress.new(order_address_params)
+    if @order_address.valid?
       pay_item
-      @order.save
+      @order_address.save
       redirect_to root_path
     else
       set_gon_public_key
@@ -35,8 +35,8 @@ class OrdersController < ApplicationController
     redirect_to root_path if @item.order.present? || current_user == @item.user
   end
 
-  def order_params
-    params.require(:order).permit(
+  def order_address_params
+    params.require(:order_address).permit(
       :postal_code, :prefecture_id, :city,
       :address, :building, :phone_number, :price
     ).merge(
@@ -50,7 +50,7 @@ class OrdersController < ApplicationController
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
       amount: @item.price,
-      card: order_params[:token],
+      card: order_address_params[:token],
       currency: 'jpy'
     )
   end
