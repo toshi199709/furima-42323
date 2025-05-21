c458e85ea8cd25b90c0c5f8791574b81edc6a7dd
toshi199709 <ayatoshi077@gmail.com>
2025-05-21 09:37:15 +0900

    モデル修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,5 +1,9 @@
 class OrderAddress
   include ActiveModel::Model
+  include ActiveModel::Attributes
+  include ActiveModel::Conversion
+  extend ActiveModel::Naming # ← form_withにmodel名を認識させる！
+
   attr_accessor :postal_code, :prefecture_id, :city, :address, :building,
                 :phone_number, :user_id, :item_id, :token
 
@@ -17,16 +21,26 @@ class OrderAddress
   validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
   validates :phone_number, format: { with: /\A\d{10,11}\z/ }
 
+  # form_withがモデルを新規作成と認識するために必要
+  def persisted?
+    false
+  end
+
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
-    Address.create(
-      postal_code: postal_code,
-      prefecture_id: prefecture_id,
-      city: city,
-      address: address,
-      building: building,
-      phone_number: phone_number,
-      order_id: order.id
-    )
+    if order.persisted?
+      Address.create(
+        postal_code: postal_code,
+        prefecture_id: prefecture_id,
+        city: city,
+        address: address,
+        building: building,
+        phone_number: phone_number,
+        order_id: order.id
+      )
+      true
+    else
+      false
+    end
   end
 end
